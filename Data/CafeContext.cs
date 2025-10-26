75b10cc7fadf2a8c9dfed354fdae84498ede136f
Olivier Goudreault <oligoudreault@gmail.com>
2025-10-26 19:27:10 -0400

    Add project files.

---
+++ Data/CafeContext.cs
@@ -0,0 +1,13 @@
+﻿using CampusCafeApp.Models;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace CampusCafeApp.Data
+{
+    public class CafeContext : DbContext
+    {
+        public CafeContext(DbContextOptions<CafeContext> options) : base(options) { }
+
+        public DbSet<MenuItem> MenuItems { get; set; } = default!;
+    }
+}
---
+++ Models/MenuItem.cs
@@ -0,0 +1,11 @@
+﻿namespace CampusCafeApp.Models
+{
+    public class MenuItem
+    {
+        public int Id { get; set; }                 // PK
+        public string Name { get; set; } = string.Empty;
+        public string Category { get; set; } = string.Empty; // e.g., "Drink", "Sandwich", "Dessert"
+        public decimal Price { get; set; }
+        public bool IsAvailable { get; set; }
+    }
+}
